95632f252a204560f09847be2cc3a47dee6c31d9
Victor Porof <victor.porof@gmail.com>
2011-06-05 17:55:46 +0300

    Using nsIConsoleService and string bundle for logging errors

--- src/chrome/content/engine/jTiltEngine.js
+++ src/chrome/content/engine/jTiltEngine.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * jTiltEngine.js - Helper low-level functions for WebGL
  * version 0.1
  *
@@ -24,23 +24,23 @@
  *    distribution.
  */
 function TiltEngine() {
-  
+
   /**
    * WebGL context to be used.
    */
   this.gl = undefined;
-  
+
   /**
    * The current shader used by the gl context.
    */
   this.shader = undefined;
-  
+
   /**
    * Initializes a WebGL context, and runs fail or success callback functions.
    */
   this.initWebGL = function(canvas, width, height,
                             successCallback, failCallback) {
-    
+
     var gl = WebGLUtils.create3DContext(canvas);
     if (gl) {
       if (width) {
@@ -60,7 +60,7 @@ function TiltEngine() {
     }
     return this.gl = gl;
   }
-  
+
   /**
    * Initializes a shader program, using sources located at a specific url.
    */
@@ -73,41 +73,43 @@ function TiltEngine() {
       fragShaderURL = vertShaderURL + ".fs";
       vertShaderURL = vertShaderURL + ".vs";
     }
-    
+
     var that = this;
     var vertShader;
     var fragShader;
-    
-    this.requests([vertShaderURL, fragShaderURL], 
+
+    this.requests([vertShaderURL, fragShaderURL],
                   function finishedCallback(http) {
-                    
+
       vertShader = that.compileShader(http[0].responseText,
                                       "x-shader/x-vertex");
-      
+
       fragShader = that.compileShader(http[1].responseText,
                                       "x-shader/x-fragment");
-      
+
       vertShader.src = http[0].responseText;
       fragShader.src = http[1].responseText;
-      
+
       if (readyCallback) {
         readyCallback(that.linkProgram(vertShader, fragShader));
       }
     });
   }
-  
+
   /**
    * Compiles a shader source of pecific type, either x-vertex or x-fragment.
    */
   this.compileShader = function(shaderSource, shaderType) {
     var gl = this.gl;
     var shader;
-    
+
     if (!shaderSource) {
-      alert("Could not create shader: shader source is null.")
+      TiltUtils.Console.error(TiltUtils.StringBundle.get(
+        "compileShader.source.error"));
+
       return null;
     }
-    
+
     if (shaderType == "x-shader/x-vertex") {
       shader = gl.createShader(gl.VERTEX_SHADER);
     }
@@ -115,20 +117,22 @@ function TiltEngine() {
       shader = gl.createShader(gl.FRAGMENT_SHADER);
     }
     else {
-      alert("Wrong shader type specified for: " + shaderSource);
+      TiltUtils.Console.error(TiltUtils.StringBundle.format(
+        "compileShader.type.error"), [shaderSource]);
+        
       return null;
     }
-    
+
     gl.shaderSource(shader, shaderSource);
     gl.compileShader(shader);
-    
+
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert(gl.getShaderInfoLog(shader));
+      TiltUtils.Console.error(gl.getShaderInfoLog(shader));
       return null;
     }
     return shader;
   }
-  
+
   /**
    * Links two compiled vertex/fragment shaders together to form a program.
    */
@@ -138,23 +142,22 @@ function TiltEngine() {
     gl.attachShader(program, vertShader);
     gl.attachShader(program, fragShader);
     gl.linkProgram(program);
-    
+
     if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      alert("Could not initialize shader program:\n\n" +
-        vertShader.src + "\n\n" +
-        fragShader.src);
+      TiltUtils.Console.error(TiltUtils.StringBundle.format(
+        "linkProgram.error", [vertShader.src, fragShader.src]));
     }
     else this.useShader(program);
     return program;
   }
-  
+
   /**
    * Uses the shader program as current for the gl context.
    */
   this.useShader = function(program) {
     this.gl.useProgram(this.shader = program);
   }
-  
+
   /**
    * Gets a shader attribute location from a program.
    */
@@ -179,26 +182,26 @@ function TiltEngine() {
     }
     return location;
   }
-  
+
   /**
    * Initializes buffer data to be used for drawing, using an array of floats.
    * The 'numItems' can be specified to use only a portion of the array.
    */
   this.initBuffer = function(elementsArray, itemSize, numItems) {
     if (!numItems) numItems = elementsArray.length / itemSize;
-    
+
     var gl = this.gl;
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(elementsArray),
                   gl.STATIC_DRAW);
-    
+
     buffer.itemSize = itemSize;
     buffer.numItems = numItems;
-    
+
     return buffer;
   }
-  
+
   /**
    * Initializez a buffer of vertex indices, using an array of unsigned ints.
    */
@@ -208,10 +211,10 @@ function TiltEngine() {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(elementsArray),
                   gl.STATIC_DRAW);
-    
+
     buffer.itemSize = 1;
     buffer.numItems = elementsArray.length;
-    
+
     return buffer;
   }
 
@@ -226,55 +229,57 @@ function TiltEngine() {
     var that = this;
     var gl = this.gl;
     var texture = gl.createTexture();
-    
+
     if ("object" === typeof(textureSource)) {
       texture.image = textureSource;
-      applyTextureImage();
+      bindTextureImage();
     }
     else if ("string" === typeof(textureSource)) {
       texture.image = new Image();
       texture.image.src = textureSource;
       texture.image.onload = function() {
-        applyTextureImage();
+        bindTextureImage();
       }
     }
-    
-    function applyTextureImage() {
-      Image.resizeToPowerOfTwo(texture.image, function resizeCallback(img) {
-        texture.image = img;
-        
+
+    /* Used internally for binding an image to a texture object */
+    function bindTextureImage() {
+      TiltUtils.Image.resizeToPowerOfTwo(texture.image,
+                                         function resizeCallback(image) {
+        texture.image = image;
+
         gl.bindTexture(gl.TEXTURE_2D, texture);
-        
+
         if (flipY) {
           gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
         }
-        
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, 
+
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
                       texture.image);
-                      
+
         that.setTextureParams(minFilter, magFilter, mipmap,
                               wrapS, wrapT, flipY);
-        
+
         if (mipmap) {
           gl.generateMipmap(gl.TEXTURE_2D);
         }
-        
+
         gl.bindTexture(gl.TEXTURE_2D, null);
-        
+
         if (readyCallback) {
           readyCallback(texture);
         }
       }, true);
     }
   }
-  
+
   /**
    * Sets texture parameters for the current binding.
    */
   this.setTextureParams = function(minFilter, magFilter, mipmap,
-                                   wrapS, wrapT) {                
+                                   wrapS, wrapT) {
     var gl = this.gl;
-    
+
     if (minFilter == "nearest") {
       gl.texParameteri(gl.TEXTURE_2D,
                        gl.TEXTURE_MIN_FILTER, gl.NEAREST);
@@ -290,11 +295,11 @@ function TiltEngine() {
 
     if (magFilter == "nearest") {
       gl.texParameteri(gl.TEXTURE_2D,
-                       gl.TEXTURE_MAG_FILTER, gl.NEAREST);        
+                       gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     }
     else if (magFilter == "linear" && mipmap) {
       gl.texParameteri(gl.TEXTURE_2D,
-                       gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_LINEAR);        
+                       gl.TEXTURE_MAG_FILTER, gl.LINEAR_MIPMAP_LINEAR);
     }
     else {
       gl.texParameteri(gl.TEXTURE_2D,
@@ -311,7 +316,7 @@ function TiltEngine() {
     }
 
     if (wrapT == "repeat") {
-      gl.texParameteri(gl.TEXTURE_2D, 
+      gl.texParameteri(gl.TEXTURE_2D,
                        gl.TEXTURE_WRAP_T, gl.REPEAT);
     }
     else {
@@ -319,7 +324,7 @@ function TiltEngine() {
                        gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     }
   }
-  
+
   /**
    * Draws specified vertex buffers, for a custom modelview and projection;
    * This function implies that default uniforms & attributes are embedded in
@@ -331,7 +336,7 @@ function TiltEngine() {
                                color,
                                texture,
                                indexBuffer, drawMode) {
-    
+
     var shader = this.shader;
     this.drawVertices_(shader.mvMatrix, mvMatrix,
                        shader.projMatrix, projMatrix,
@@ -341,7 +346,7 @@ function TiltEngine() {
                        shader.sampler, texture,
                        indexBuffer, drawMode);
   }
-  
+
   /**
    * Draws specified vertex buffers, for a custom modelview and projection;
    * This function does not imply anything. Buffer parameters can be omitted.
@@ -355,10 +360,10 @@ function TiltEngine() {
                                 textureSampler, texture,
                                 indexBuffer, drawMode) {
     var gl = this.gl;
-    
+
     gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
     gl.uniformMatrix4fv(projMatrixUniform, false, projMatrix);
-    
+
     if (verticesAttribute != undefined && verticesBuffer != undefined) {
       gl.enableVertexAttribArray(verticesAttribute);
       gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
@@ -371,7 +376,7 @@ function TiltEngine() {
       gl.vertexAttribPointer(texcoordAttribute, texcoordBuffer.itemSize,
                              gl.FLOAT, false, 0, 0);
     }
-    if (colorUniform != undefined && color != undefined) {      
+    if (colorUniform != undefined && color != undefined) {
       gl.uniform4f(colorUniform, color[0], color[1], color[2], color[3]);
     }
     if (textureSampler != undefined && texture != undefined) {
@@ -391,7 +396,7 @@ function TiltEngine() {
       gl.drawArrays(drawMode, 0, verticesBuffer.numItems);
     }
   }
-  
+
   /**
    * Handles a generic get request, performed on a specified url. When done,
    * it fires the ready callback function if it exists, & passes an optional
@@ -399,7 +404,7 @@ function TiltEngine() {
    */
   this.request = function(url, readyCallback, auxParam) {
     var http = new XMLHttpRequest();
-    
+
     http.open("GET", url, true);
     http.send(null);
     http.onreadystatechange = function() {
@@ -410,7 +415,7 @@ function TiltEngine() {
       }
     };
   }
-  
+
   /**
    * Handles multiple get requests from specified urls. When all requests are
    * completed, it fires the ready callback function if it exists & passes an
@@ -419,7 +424,7 @@ function TiltEngine() {
   this.requests = function(urls, readyCallback, auxParam) {
     var http = [];
     var finished = 0;
-    
+
     function onrequestready() {
       finished++;
       if (finished == urls.length) {
@@ -428,7 +433,7 @@ function TiltEngine() {
         }
       }
     }
-    
+
     for (var i = 0, size = urls.length; i < size; i++) {
       this.request(urls[i], function ready(request, index) {
         http[index] = request;
@@ -436,85 +441,4 @@ function TiltEngine() {
       }, i);
     }
   }
-}
-
-/**
- * Scales an image to power of two width and height.
- */
-Image.resizeToPowerOfTwo = function(image, readyCallback, forceResize) {
-  if (Math.isPowerOfTwo(image.width) &&
-      Math.isPowerOfTwo(image.height) && !forceResize) {
-
-    if (readyCallback) {
-      readyCallback(image);
-      return;
-    }
-  }
-
-  var iframe_id = "tilt-iframe-" + image.src;
-  var canvas_id = "tilt-canvas-" + image.src;
-
-  TiltIframe.Utils.initCanvas(function loadCallback(iframe, canvas) {
-    canvas.width = Math.nextPowerOfTwo(image.width);
-    canvas.height = Math.nextPowerOfTwo(image.height);
-
-    var context = canvas.getContext('2d');
-    context.drawImage(image, 
-      0, 0, image.width, image.height,
-      0, 0, canvas.width, canvas.height);
-
-    if (readyCallback) {
-      readyCallback(image = canvas);
-    }
-  }, false, iframe_id, canvas_id);
-}
-
-/**
- * Helper function, converts degrees to radians.
- */
-Math.radians = function(degrees) {
-  return degrees * Math.PI / 180;
-}
-
-/**
- * Returns if parameter is a power of two.
- */
-Math.isPowerOfTwo = function (x) {
-  return (x & (x - 1)) == 0;
-}
-
-/**
- * Returns the next highest power of two for a number.
- */
-Math.nextPowerOfTwo = function(x) {
-  --x;
-  for (var i = 1; i < 32; i <<= 1) {
-    x = x | x >> i;
-  }
-  return x + 1;
-}
-
-/**
- * Converts a hex color to rgba.
- */
-Math.hex2rgba = function(hex) {
-  hex = hex.charAt(0) == "#" ? hex.substring(1, 7) : hex;
-  
-  if (hex.length == 3) {
-    hex = hex.charAt(0) + hex.charAt(0) +
-          hex.charAt(1) + hex.charAt(1) +
-          hex.charAt(2) + hex.charAt(2);
-  }
-  else if (hex.match("^rgba") == "rgba") {
-    return hex.substring(5, hex.length - 1).split(',');
-  }
-  else if (hex.match("^rgb") == "rgb") {
-    return hex.substring(4, hex.length - 1).split(',');
-  }
-  
-  var r = parseInt(hex.substring(0, 2), 16) / 255;
-  var g = parseInt(hex.substring(2, 4), 16) / 255;
-  var b = parseInt(hex.substring(4, 6), 16) / 255;
-  
-  return [r, g, b, 1];
 }
