7abeb61fdfeee2d163aadddc1fe41fc156af1d81
Victor Porof <victor.porof@gmail.com>
2011-07-12 11:08:46 +0300

    Declaring all the variables seems necessary for minification to work properly

--- src/chrome/content/engine/lib/glMatrix-0.9.6.js
+++ src/chrome/content/engine/lib/glMatrix-0.9.6.js
@@ -26,7 +26,7 @@
  *    distribution.
  */
 
-var glMatrixArrayType;
+var glMatrixArrayType, vec3, mat3, mat4, quat4;
 
 // Fallback for systems that don't support WebGL
 if(typeof Float32Array != 'undefined') {
