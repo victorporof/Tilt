f3e0d504b345d385903a1ac5a52596df8dc5e1b0
Victor Porof <victor.porof@gmail.com>
2011-07-13 20:51:16 +0300

    Various helper functions for XMLHttpRequest

---
+++ src/chrome/content/engine/utils/Xhr.js
@@ -0,0 +1,91 @@
+/*
+ * Xhr.js - Various helper functions for XMLHttpRequest
+ * version 0.1
+ *
+ * Copyright (c) 2011 Victor Porof
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ *
+ *    3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+"use strict";
+
+var Tilt = Tilt || {};
+var EXPORTED_SYMBOLS = ["Tilt.Xhr"];
+
+/** 
+ * XMLHttpRequest utilities.
+ */
+Tilt.Xhr = {
+
+  /**
+   * Handles a generic get request, performed on a specified url. When done,
+   * it fires the ready callback function if it exists, & passes the http
+   * request object and also an optional auxiliary parameter if available.
+   * Used internally for getting shader sources from a specific resource.
+   *
+   * @param {String} url: the url to perform the GET to
+   * @param {Function} readyCallback: function to be called when request ready
+   * @param {Object} aParam: optional parameter passed to readyCallback
+   */
+  request: function(url, readyCallback, aParam) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url, true);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if ("function" === typeof readyCallback) {
+          readyCallback(xhr, aParam);
+        }
+      }
+    };
+  },
+
+  /**
+   * Handles multiple get requests from specified urls. When all requests are
+   * completed, it fires the ready callback function if it exists, & passes
+   * the http request object and an optional auxiliary parameter if available.
+   * Used internally for getting shader sources from a specific resource.
+   *
+   * @param {Array} urls: an array of urls to perform the GET to
+   * @param {Function} readyCallback: function called when all requests ready
+   * @param {Object} aParam: optional parameter passed to readyCallback
+   */
+  requests: function(urls, readyCallback, aParam) {
+    var xhrs = [], finished = 0, i, length;
+
+    function requestReady() {
+      finished++;
+      if (finished === urls.length) {
+        if ("function" === typeof readyCallback) {
+          readyCallback(xhrs, aParam);
+        }
+      }
+    }
+
+    function requestCallback(xhr, index) {
+      xhrs[index] = xhr;
+      requestReady();
+    }
+
+    for (i = 0, length = urls.length; i < length; i++) {
+      this.request(urls[i], requestCallback, i);
+    }
+  }
+};
