10842bbb928d6be03a6797eb5385da6e665df6aa
Victor Porof <victor.porof@gmail.com>
2011-07-20 15:57:18 +0300

    Better node handling in the getNodeCoordinates function

--- src/chrome/content/engine/utils/Document.js
+++ src/chrome/content/engine/utils/Document.js
@@ -194,6 +194,9 @@ Tilt.Document = {
    */
   getNodeCoordinates: function(node) {
     try {
+      if (node.localName === "head" || node.localName === "body")
+        throw new Exception();
+
       // this is the preferred way of getting the bounding client rectangle
       var clientRect = node.getBoundingClientRect();
 
