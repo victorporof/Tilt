65e09bd3c468fb652c5c4220aab3ab69ff4d1924
Victor Porof <victor.porof@gmail.com>
2011-07-21 09:11:04 +0300

    Tilt-content.js is now Tilt-extensio.js

--- src/chrome/content/browserEntryPoint.js
+++ src/chrome/content/browserEntryPoint.js
@@ -44,7 +44,7 @@ TiltChrome.EntryPoint = {
 
     // load everything after a while, don't slow down the browser startup
     window.setTimeout(function() {
-      scriptLoader.loadSubScript("chrome://tilt/content/Tilt-content.js");
+      scriptLoader.loadSubScript("chrome://tilt/content/Tilt-extension.js");
     }, 500);
   }()
 };
