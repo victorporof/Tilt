3809db8b21dd7858ac324aa22111eeded9f4c4be
Victor Porof <victor.porof@gmail.com>
2011-07-22 08:57:12 +0300

    Better handling the double click events

--- src/chrome/content/TiltChromeController.js
+++ src/chrome/content/TiltChromeController.js
@@ -173,11 +173,6 @@ TiltChrome.Controller.MouseAndKeyboard = function() {
 
     var absX = Math.abs(mouseStartX - mouseX);
     var absY = Math.abs(mouseStartY - mouseY);
-
-    if (absX < 2 && absY < 2) {
-      this.visualization.click(mouseX, mouseY);
-    }
-
     mouseX = e.clientX - e.target.offsetLeft;
     mouseY = e.clientY - e.target.offsetTop;
     mouseStartX = mouseX;
@@ -186,6 +181,10 @@ TiltChrome.Controller.MouseAndKeyboard = function() {
     dragY = translationY;
 
     mouseDragged = false;
+
+    if (absX < 2 && absY < 2) {
+      this.visualization.click(mouseX, mouseY);
+    }
   }.bind(this);
 
   /**
@@ -198,10 +197,6 @@ TiltChrome.Controller.MouseAndKeyboard = function() {
     var absX = Math.abs(mouseStartX - mouseX);
     var absY = Math.abs(mouseStartY - mouseY);
 
-    if (absX < 2 && absY < 2) {
-      this.visualization.doubleClick(mouseX, mouseY);
-    }
-
     mouseX = e.clientX - e.target.offsetLeft;
     mouseY = e.clientY - e.target.offsetTop;
     mouseStartX = mouseX;
@@ -210,6 +205,10 @@ TiltChrome.Controller.MouseAndKeyboard = function() {
     dragY = translationY;
 
     mouseDragged = false;
+
+    if (absX < 2 && absY < 2) {
+      this.visualization.doubleClick(mouseX, mouseY);
+    }
   }.bind(this);
 
   /**
