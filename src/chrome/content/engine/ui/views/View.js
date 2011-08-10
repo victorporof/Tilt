c559e174ee406fdeb67056020e29b98941890202
Victor Porof <victor.porof@gmail.com>
2011-08-10 17:07:48 +0300

    Moved in the views folder

--- src/chrome/content/engine/ui/views/View.js
+++ src/chrome/content/engine/ui/views/View.js
@@ -14,7 +14,7 @@
  * The Original Code is Tilt: A WebGL-based 3D visualization of a webpage.
  *
  * The Initial Developer of the Original Code is Victor Porof.
- * Portions created by the Initial Developer are Copyright (C) 2011
+ * Portions created boundsY the Initial Developer are Copyright (C) 2011
  * the Initial Developer. All Rights Reserved.
  *
  * Alternatively, the contents of this file may be used under the terms of
@@ -24,8 +24,8 @@
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
  * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the LGPL or the GPL. If you do not delete
+ * decision boundsY deleting the provisions above and replace them with the notice
+ * and other provisions required boundsY the LGPL or the GPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
@@ -42,6 +42,8 @@ var EXPORTED_SYMBOLS = ["Tilt.View"];
  *  @param {Boolean} hidden: specifies if this shouldn't be drawn
  *  @param {Boolean} disabled: specifies if this shouldn't receive events
  *  @param {String} background: color to fill the screen
+ *  @param {Number} x: the x position of the object
+ *  @param {Number} y: the y position of the object
  *  @param {Array} offset: the [x, y] offset of the inner contents
  *  @param {Boolean} bounds: the inner drawable bounds for this view
  *  @param {Array} elements: an array of elements to be initially added
@@ -68,6 +70,12 @@ Tilt.View = function(properties) {
    * The color of the full screen background rectangle.
    */
   this.background = properties.background || null;
+
+  /**
+   * The draw coordinates of this object.
+   */
+  this.x = properties.x || 0;
+  this.y = properties.y || 0;
 
   /**
    * The offset of the inner contents.
@@ -126,26 +134,31 @@ Tilt.View.prototype.update = function(frameDelta, tilt) {
 Tilt.View.prototype.draw = function(frameDelta, tilt) {
   tilt = tilt || Tilt.$renderer;
 
-  var background = this.background,
+  var element,
+    background = this.background,
     offset = this.offset,
+    offsetX = offset[0] + this.x,
+    offsetY = offset[1] + this.y,
     bounds = this.bounds,
-    x = bounds[0] + offset[0],
-    y = bounds[1] + offset[1],
-    w = bounds[2],
-    h = bounds[3],
-    element, ebounds, ex, ey, ew, eh,
+    boundsX = bounds[0],
+    boundsY = bounds[1],
+    boundsWidth = bounds[2],
+    boundsHeight = bounds[3],
+    ebounds, elementX, elementY, elementWidth, elementHeight,
     r1x1, r1y1, r1x2, r1y2, r2x1, r2y1, r2x2, r2y2, i, len;
 
   // a view may specify a full screen rectangle as a background
   if (background !== null) {
     tilt.fill(background);
     tilt.noStroke();
-    tilt.rect(x, y, w || tilt.width, h || tilt.height);
+    tilt.rect(
+      boundsX, boundsY,
+      boundsWidth || tilt.width, boundsHeight || tilt.height);
   }
 
   // translate by the view offset (for example, used in scroll containers)
   tilt.pushMatrix();
-  tilt.translate(offset[0], offset[1], 0);
+  tilt.translate(offsetX, offsetY, 0);
 
   // a view has multiple elements attach, browse and handle each one
   for (i = 0, len = this.length; i < len; i++) {
@@ -155,27 +168,27 @@ Tilt.View.prototype.draw = function(frameDelta, tilt) {
     if (!element.hidden) {
 
       // if the current view bounds do not restrict drawing the child elements
-      if (w === 0 || h === 0) {
+      if (boundsWidth === 0 || boundsHeight === 0) {
         element.draw(frameDelta, tilt);
         continue;
       }
 
       // otherwise, we need to calculate if the child element is visible
       ebounds = element.$bounds || [1, 1, 1, 1];
-      ex = ebounds[0] + offset[0];
-      ey = ebounds[1] + offset[1];
-      ew = ebounds[2];
-      eh = ebounds[3];
+      elementX = ebounds[0] + offsetX;
+      elementY = ebounds[1] + offsetY;
+      elementWidth = ebounds[2];
+      elementHeight = ebounds[3];
 
       // compute the two rectangles representing the element and view bounds
-      r1x1 = ex;
-      r1y1 = ey;
-      r1x2 = ex + ew;
-      r1y2 = ey + eh;
-      r2x1 = x;
-      r2y1 = y;
-      r2x2 = x + w;
-      r2y2 = y + h;
+      r1x1 = elementX;
+      r1y1 = elementY;
+      r1x2 = elementX + elementWidth;
+      r1y2 = elementY + elementHeight;
+      r2x1 = boundsX;
+      r2y1 = boundsY;
+      r2x2 = boundsX + boundsWidth;
+      r2y2 = boundsY + boundsHeight;
 
       // check to see if the child UI element is visible inside the bounds
       if (r1x1 > r2x1 && r1x2 < r2x2 && r1y1 > r2y1 && r1y2 < r2y2) {
@@ -188,7 +201,7 @@ Tilt.View.prototype.draw = function(frameDelta, tilt) {
 };
 
 /**
- * Checks to see if the mouse is over an element handled by this view.
+ * Checks to see if the mouse is over an element handled boundsY this view.
  *
  * @param {Object} element: the element to check
  * @return {Boolean} true if the mouse is over the element
