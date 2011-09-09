4063b0b6ee3a6e3c87863e310d76f0e7cb895c21
Victor Porof <victor.porof@gmail.com>
2011-09-09 19:58:48 +0300

    Latest build added

--- bin/Tilt-extension.js
+++ bin/Tilt-extension.js
@@ -13190,7 +13190,7 @@ TiltChrome.Visualization = function(canvas, controller, ui) {
 
       // show a corresponding prompt message and open a tab to troubleshooting
       gBrowser.selectedTab =
-        gBrowser.addTab("http://get.webgl.org/troubleshooting/");
+        gBrowser.addTab("http://get.webgl.org/");
     }
   }),
 
@@ -13565,14 +13565,14 @@ TiltChrome.Visualization = function(canvas, controller, ui) {
         // they can be reused from the bottom and top faces; we do, however,
         // duplicate some vertices from front face, because it has custom
         // texture coordinates which are not shared by the other faces
-                        x,     y + h, z - thickness,       /* top */    // 4
-                        x + w, y + h, z - thickness,                    // 5
-                        x + w, y + h, z,                                // 6
-                        x,     y + h, z,                                // 7
-                        x,     y,     z,                   /* bottom */ // 8
-                        x + w, y,     z,                                // 9
-                        x + w, y,     z - thickness,                    // 10
-                        x,     y,     z - thickness);                   // 11
+                        x,     y + h, z - thickness,        /* top */    // 4
+                        x + w, y + h, z - thickness,                     // 5
+                        x + w, y + h, z,                                 // 6
+                        x,     y + h, z,                                 // 7
+                        x,     y,     z,                    /* bottom */ // 8
+                        x + w, y,     z,                                 // 9
+                        x + w, y,     z - thickness,                     // 10
+                        x,     y,     z - thickness);                    // 11
 
         // compute the texture coordinates
         texCoord.unshift((x + tilt.width  * 0.5    ) / texture.width,
@@ -13696,12 +13696,15 @@ TiltChrome.Visualization = function(canvas, controller, ui) {
     // when the tab is closed or the url changes, destroy visualization
     tabContainer.addEventListener("TabClose", gClose, false);
     tabContainer.addEventListener("TabAttrModified", gClose, false);
+
+    // some other miscellaneous events handling blur, focus, resize etc.
     contentWindow.addEventListener("blur", gBlur, false);
     contentWindow.addEventListener("focus", gFocus, false);
     contentWindow.addEventListener("resize", gResize, false);
     gBrowser.addEventListener("mouseover", gMouseOver, false);
     gBrowser.addEventListener("load", gLoad, true);
 
+    // internal events triggered by the popups used for the user interface
     sourceEditor.addEventListener("popupshown", eEditorShown, false);
     sourceEditor.addEventListener("popuphidden", eEditorHidden, false);
     colorPicker.addEventListener("popupshown", ePickerShown, false);
@@ -13712,36 +13715,47 @@ TiltChrome.Visualization = function(canvas, controller, ui) {
    * Event handling the MozAfterPaint event.
    */
   var gAfterPaint = function(e) {
+    // don't refresh while resizing, focusing or unfocusing the window
     if (this.$gResizing || this.$gFocusing || this.$gBlurring) {
       return;
     }
 
+    // cache some necessary variables, like boundings, offsets etc.
     var boundingClientRect = e.boundingClientRect,
-      offsetLeft = canvas.offsetLeft,
-      offsetTop = canvas.offsetTop,
-      innerWidth = window.content.innerWidth,
-      innerHeight = window.content.innerHeight,
       left = boundingClientRect.left,
       top = boundingClientRect.top,
       width = boundingClientRect.width,
-      height = boundingClientRect.height;
+      height = boundingClientRect.height,
+      offsetLeft = canvas.offsetLeft,
+      offsetTop = canvas.offsetTop,
+      innerWidth, innerHeight;
 
+    // the refreshed area is inside the window content rectangle
     if (top > offsetTop && left > offsetLeft && width > 4 && height > 4) {
       this.requestRefreshTexture(boundingClientRect);
     }
-    else if (left <= 0 && top <= 0 &&
-            (left >= -innerWidth || top >= -innerHeight) &&
-            (width >= innerWidth || height >= innerHeight)) {
+    // the entire dom tree has likely changed, so refresh everything
+    else {
+      innerWidth = window.content.innerWidth;
+      innerHeight = window.content.innerHeight;
 
-      window.setTimeout(function() {
-        try {
-          if (!refreshMesh && !refreshTexture) {
-            this.requestRefreshTexture(null);
-            this.requestRefreshMesh();
+      if (left <= 0 && top <= 0 &&
+         (left >= -innerWidth || top >= -innerHeight) &&
+         (width >= innerWidth || height >= innerHeight)) {
+
+        // multiple refresh events could be triggered at the same time, so in
+        // order to minimize the reinitialization calls, set a timeout and
+        // check if the mesh or texture is currently refreshed
+        window.setTimeout(function() {
+          try {
+            if (!refreshMesh && !refreshTexture) {
+              this.requestRefreshTexture(null);
+              this.requestRefreshMesh();
+            }
           }
-        }
-        catch(e) {}
-      }.bind(this), 10);
+          catch(e) {}
+        }.bind(this), 10);
+      }
     }
   }.bind(this);
 
@@ -14457,7 +14471,7 @@ TiltChrome.Visualization = function(canvas, controller, ui) {
       gAfterPaint = null;
     }
     if (gDeviceMotion !== null) {   
-      window.addEventListener("devicemotion", gDeviceMotion, false);
+      window.removeEventListener("devicemotion", gDeviceMotion, false);
       gDeviceMotion = null;
     }
     if (gClose !== null) {
